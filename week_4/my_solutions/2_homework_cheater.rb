002df925a38196d85b69d40af2cc2c3bf1129837
Clark Hinchcliff <clarkhinchcliff@hotmail.com>
2014-04-02 15:31:10 -0700

    2 and 3
    Finished 2 & 3

--- week_4/my_solutions/2_homework_cheater.rb
+++ week_4/my_solutions/2_homework_cheater.rb
@@ -5,29 +5,68 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: title, author, date, thesis, character, quality, and pronoun
+# Output: I recently read "Title" by "Author" which was written back in "Date". "Thesis" (with pronouns intermixed in thesis). The quality I admire most of "character" is "pronoun" "quality"
+# Steps: 
 
+=begin
+	1. Define a method called essay_template that takes the arguments title, author, date, thesis, and pronoun
+	2. Create a filler statement that takes in the arguments
+	3. Define pronouns, put in appropriate he/she, him/her, his/hers with if else statements
+	
+=end
 
 # 3. Initial Solution
 
-
-
-
+def essay_template(title, author, date, thesis, character, quality, moment, pronoun)
+	if pronoun == "male"
+		possessive = "his"
+		subject = "he"
+		object = "him"
+	elsif pronoun == "female"
+		possessive = "her"
+		subject = "she"
+		object = "her"
+	else
+		possessive = "its"
+		subject = "it"
+		object = "it"
+	end
+	cap_title = title.split.map(&:capitalize).join(' ')
+	last_name = character.split.last
+	puts "Recently, for class, we read #{cap_title}. #{cap_title} was written by #{author.split.map(&:capitalize).join(' ')} and published on #{date}. #{thesis.capitalize}. The quality of the main character, #{author.split.map(&:capitalize).join(' ')}, that I found most endearing was #{possessive} quality of #{quality}. #{last_name.capitalize} had shown a good example of #{possessive} #{quality} when #{subject} #{moment}. I appreciated #{object} and #{possessive} #{quality}."
+end
 # 4. Refactored Solution
 
-
-
-
-
-
-
+def essay_template(title, author, date, thesis, character, quality, moment, pronoun)
+	if pronoun == "male"
+		possessive = "his"
+		subject = "he"
+		object = "him"
+	elsif pronoun == "female"
+		possessive = "her"
+		subject = "she"
+		object = "her"
+	else
+		possessive = "its"
+		subject = "it"
+		object = "it"
+	end
+	cap_title = title.split.map(&:capitalize).join(' ')
+	last_name = character.split.last
+	puts "Recently, for class, we read #{cap_title}. #{cap_title} was written by #{author.split.map(&:capitalize).join(' ')} and published on #{date}. #{thesis.capitalize}. The quality of the main character, #{author.split.map(&:capitalize).join(' ')}, that I found most endearing was #{possessive} quality of #{quality}. #{last_name.capitalize} had shown a good example of #{possessive} #{quality} when #{subject} #{moment}. I appreciated #{object} and #{possessive} #{quality}."
+end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+him_essay = "Recently, for class, we read Bacon. Bacon was written by Bacon Jones and published on 1/4/2014. It was a good book. The quality of the main character, Bacon Jones, that I found most endearing was his quality of sandwich eating. Picard had shown a good example of his sandwich eating when he punched a leprechaun and ate his sandwich. I appreciated him and his sandwich eating."
+her_essay = "Recently, for class, we read Hamster. Hamster was written by Nora Jean and published on 1,2,3213. I like to eat cheese. The quality of the main character, Nora Jean, that I found most endearing was her quality of werewolf riding. Ann had shown a good example of her werewolf riding when she rode a werewolf into town. I appreciated her and her werewolf riding."
+it_essay = "Recently, for class, we read Nothing. Nothing was written by Count Spoon and published on 2/12/4112. It was better than i expected. The quality of the main character, Count Spoon, that I found most endearing was its quality of barking. Dog had shown a good example of its barking when it barked at the mailman. I appreciated it and its barking."
 
-
-
+puts her_essay == essay_template("hamster", "nora jean", "1,2,3213", "i like to eat cheese", "lily ann", "werewolf riding", "rode a werewolf into town", "female")
+puts
+puts him_essay == essay_template("bacon", "bacon jones", "1/4/2014", "It was a good book", "james picard", "sandwich eating", "punched a leprechaun and ate his sandwich", "male")
+puts
+puts it_essay == essay_template("nothing", "count spoon", "2/12/4112", "it was better than I expected", "iggy dog", "barking", "barked at the mailman", "non-human")
 
 
 
@@ -36,13 +75,26 @@
 =begin
 	
 What parts of your strategy worked? What problems did you face?
+	my driver code keeps coming out as false for some reason, even though the answer is exactly the same.
+	everything else was easy as all get out
+
 What questions did you have while coding? What resources did you find to help you answer them?
+	No questions, I understood this one.
+
 What concepts are you having trouble with, or did you just figure something out? If so, what?
+	Driver code. Need to research more.
+
 Did you learn any new skills or tricks?
+	how to split with map, capitalize all first letters and then rejoin.
+
 How confident are you with each of the learning objectives?
+	pretty confident.
+
 Which parts of the challenge did you enjoy?
+	It was fun to make silly stories.
+
 Which parts of the challenge did you find tedious?
-	
+	Driver code.
 =end
 
 
---
+++ week_4/my_solutions/3_commas_solution.rb
@@ -0,0 +1,79 @@
+# U2.W4: Title here
+
+
+# I worked on this challenge [by myself].
+
+# 2. Pseudocode
+
+# Input: any number
+# Output: a number that has every three digits separated from the rest with commas
+# Steps:
+=begin
+
+	1. Define a comma_separator method that takes number as it's argument
+	2. convert the number to a string or we will be unable to use .chars
+	3. put all of the characters in the newly created string into an array
+	4. reverse the order of the array so the commas are put in correctly (otherwise they're put in backwards)
+	5. each_slice with an argument of 3 so the commas are joined after every 3rd. (this creates arrays of three)
+	6. use the .map function with &join as its argument (else we'll get all numbers separated by commas) 
+	7. Then join everything together so there are only commas after every 3 digits (if we don't do this we'll have several reverse arrays with 3 digits only separated by commas. This joins the rest of the arrays together and does the actual separating with commas put in as the argument)
+	8. re-reverse everything so everything shows up in the right order.
+		
+=end
+
+# 3. Initial Solution
+
+def separate_comma(number)
+	string = number.to_s
+	big_array = string.chars.to_a
+	reversed = big_array.reverse
+	sliced = reversed.each_slice(3)
+	joined = sliced.map(&:join).join(",")
+	finished_product = joined.reverse
+	return finished_product
+end
+
+
+# 4. Refactored Solution
+
+def separate_comma(number)
+	number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
+end
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+#see 3_commas_spec.rb
+
+
+
+
+# 5. Reflection 
+
+=begin
+	
+What parts of your strategy worked? What problems did you face?
+	I tried to initially just add in commas after every 3 digits after converting everything to a string, but that put the commas in the wrong places.
+	After a while I realized it was putting my commas in the reverse order. I found online the .reverse method and was able to figure out that I could just have commas put in after every 3 in the reverse order.
+	Unfortunately that returned every number backwards, I tried reversing them again, and everything worked out fine.
+
+What questions did you have while coding? What resources did you find to help you answer them?
+	I used the official documentation. I just needed to learn more methods to do things.
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+	None here. This was pretty easy.
+
+Did you learn any new skills or tricks?
+	I learned that .chars separates all of the characters of strings.
+	I learned that I could use .reverse twice to get everything I want done without having to resort to using the length of the array and then dividing the max characters by 3 to decide the amount of commas I will need
+	I hadn't used the map function until now, and I found it useful with regards to getting everything connected in the incepted arrays
+
+How confident are you with each of the learning objectives?
+	Pretty confident here
+
+Which parts of the challenge did you enjoy?
+	These challenges are like puzzles and I just like them.
+
+Which parts of the challenge did you find tedious?
+	None.
+
+=end
