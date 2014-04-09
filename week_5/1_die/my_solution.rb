b0a48f4c436dc4817dd01dd69ab5915c2520db01
Clark Hinchcliff <clarkhinchcliff@hotmail.com>
2014-04-09 08:13:29 -0600

    Gps 2.1 and Solution 4.1
    Uploads for GPS 2.1 and Solution for 4.1

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,45 +1,88 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: A die class that takes in a die roll method with any given number of sides (must be greater than 1)
+# Output: A random number from 1 to the maximum sides of the die (technically a die couldn't have just one side)
+# Steps: 
+	# 1. Create a Die Class
+	# 2. Set a class variable of sides equal to the sides in our initialize method
+	# 3. raise an ArgumentError when an amount of sides less than one is put in
+	# 4. define a sides method with the class variable of sides
+	# 5. define a roll method that will give us a random number between one and the amount of sides
 
 # 3. Initial Solution
 
-class Die
-  def initialize(sides)
-    # code goes here
-  end
+# class Die
+#   def initialize(sides)
+#     @sides = sides
+#     if sides < 1
+#     	raise ArgumentError.new("There cannot be less than one side on a die.")
+#     end
+#   end
   
-  def sides
-    # code goes here
-  end
+#   def sides
+#     @sides
+#   end
   
-  def roll
-    # code goes here
-  end
-end
+#   def roll
+#     rand(@sides) + 1
+#   end
+# end
 
 
 
 # 4. Refactored Solution
 
-
+class Die
+  def initialize(sides)
+    @sides = sides
+    raise ArgumentError.new("There cannot be less than one side on a die.") if sides < 1
+  end
+  
+  def sides
+    @sides
+  end
+  
+  def roll
+    rand(@sides) + 1
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
-
-
-
+p Die.new(5).sides == 5 #=> Displays whether or not we have the correct amount of sides
+p Die.new(17).roll.between?(1,17) #=> Will display whther or not we have the correct range for the roll
+p Die.instance_method(:initialize).arity == 1  #=> Shows whether or not our initialize method only takes one argument
+p Die.instance_method(:sides).arity == 0 #=> shows that our sides method takes in no arguments
+p Die.instance_method(:roll).arity == 0 #=> shows that our roll method takes no argument
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face?
+#   The 1_die_spec.rb file did not run for me, so I just read it
+#   I had issues finding ways to make driver code for the amount of arguments, but research helped me find some new methods.
+#   Other than the driver code, the rest of the solution was very easy do to. In reviewing others' code, I see that most of them used rand(1..@sides) which works, but mine works just as well, so I didn't bother to change it.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+#   I had a few questions, but they were all answered with new methods I found in the documentation
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+#   Driver code is something that I have to put extra effor in doing, but I'll be okay.
+
+# Did you learn any new skills or tricks?
+#   I learned the .instance_method module as well as .arity
+
+# How confident are you with each of the learning objectives?
+#   Everything is getting easier as we go on
+
+# Which parts of the challenge did you enjoy?
+#   I think all of the challenges are great. I enjoy being pushed.
+
+# Which parts of the challenge did you find tedious?
+#   None, aside from there not being extra resources for driver code specific modules and methods.
--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,22 +1,50 @@
-# U2.W5: Bakery Challenge GPS
+# U2.W5: The Bakery Challenge (GPS 2.1)
 
-# I worked on this challenge with: 
+# Your Names
+# 1)Clark Hinchcliff
+# 2)
 
+ # This is the file you should end up editing. 
+ 
+def bakery_num(num_of_people, fav_food)
+  my_list = {
+             "pie" => 8, 
+             "cake" => 6, 
+             "cookie" => 1
+             }
+             #{pie: 8} == {:pie => 8}
+             #{"pie" => 8}
+  
+  has_fav_food = false
 
+  my_list.each_key do |list_item|
+    if list_item == fav_food
+      has_fav_food = true
+    end
+  end
+  
+  if has_fav_food == false
+    raise ArgumentError.new("You can't make that food")
+  else
+    fav_food_qty = my_list.values_at(fav_food)[0]
+    if num_of_people % fav_food_qty == 0
+      num_of_food = num_of_people / fav_food_qty
+      return "You need to make #{num_of_food} #{fav_food}(s)."
+    else
+      while num_of_people > 0
+        quant = {}
+        my_list.each do |list_item, list_item_val|
+          quant[list_item] = num_of_people / list_item_val
+          num_of_people = num_of_people % list_item_val
+        end
+      end
+      return "You need to make #{quant["pie"]} pie(s), #{quant["cake"]} cake(s), and #{quant["cookie"]} cookie(s)."
+    end
+  end
+end
+ 
 
-
-
-
-# Our Refactored Solution
-
-
-
-
-
-
-
-
-
+#-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
 # These are the tests to ensure it's working. 
 # These should all print true if the method is working properly.
@@ -29,11 +57,4 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
 p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
-
-
-
-
-#  Reflection 
-
-
-
+ 
